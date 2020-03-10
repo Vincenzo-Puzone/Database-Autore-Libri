8faf7e266331b4c3eb3cbcbb0c17c5d35a7faf16
Vincenzo Puzone <vincy122679@libero.it>
2020-03-10 12:20:25 +0100

    adding things

--- index.js
+++ index.js
@@ -15,16 +15,21 @@ app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 app.use('/css', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use( morgan('dev') );
+const db = new sqlite3.Database('./test.db',()=>{
+    app.listen(8080);
+    console.log('Server running on http://localhost:8080');
+    console.log('database open')
+});
 
 app.get('/',(req,res)=>{
     res.send('Ciao');
+    sqlA='select * from Autori';
+    sqlL='select * from Libri';
+    db.all(sqlA,(err,rows)=>console.log(rows))
+    db.all(sqlL,(err,rows)=>console.log(rows))
 })
 
 app.use((req,res)=>{
     res.status(404);
     res.sendFile(path.join(__dirname,'public','404.html'));
 })
-
-app.listen(8080);
-
-console.log('Server running on http://localhost:8080');
